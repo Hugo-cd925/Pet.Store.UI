17f04eefac67aaa117e6f956a98a6309627b4143
Hugo Delgado <hugo.cdelgado925@gmail.com>
2023-04-05 10:45:23 -0700

    Add Http Helper with Constants for API Rpute

---
+++ src/utility/constants.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    API_ERROR: 'Oops, something went wrong',
+    ALL_PRODUCTS_ENDPOINT: 'https://fakestoreapi.com/products',
+    ALL_CATEGORIES_ENDPOINT:'https://fakestoreapi.com/products/categories',
+    ALL_CARTS_ENDPONT:'https://fakestoreapi.com/carts',
+    ALL_USERS_ENDPONT:'https://fakestoreapi.com/users'
+});
---
+++ src/utility/httphelper.js
@@ -0,0 +1,17 @@
+import constants from './Constants';
+
+/**
+ * @name HttpHelper
+ * @description - Utility method for using fetch in a convenient manner
+ * @param {string} route address to ping
+ * @param {string} method http method
+ * @param {Object} payload object to send
+ * @return {Promise} - Promise from the fetch call
+ */
+export default (route, method, payload) => fetch(constants.BASE_URL_API + route, {
+  method,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(payload)
+});
